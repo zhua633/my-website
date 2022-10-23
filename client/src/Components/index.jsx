9d16a42691a1ae057d32462ea3808112bbbac83c
zhua633 <zhua633@aucklanduni.ac.nz>
2022-10-23 21:22:12 +1300

    added some strucutre

---
+++ client/src/Components/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "./Nav";
+import Hero from "./Hero";
+
+
+const Home = () => {
+    return (
+        <>
+            <Navbar/>
+            <Hero/>
+        </>
+    )
+}
+
+export default Home;
