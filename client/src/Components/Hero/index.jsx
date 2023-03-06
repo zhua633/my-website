6c5389654efdd593c9599176a974b47c491bf873
zhua633 <zhua633@aucklanduni.ac.nz>
2023-03-06 19:41:36 +1300

    Altered hero icon spacing

--- client/src/Components/Hero/index.jsx
+++ client/src/Components/Hero/index.jsx
@@ -1,30 +1,27 @@
 import React from 'react'
 import './index.css'
 
-// function HelloWorld(props){
-//     return(
-//         <h1>Hello {props.name}!</h1>
-//     )
-// }
-
 class Hero extends React.Component{
     render(){
         return(
             <section className='hero'>
             <div className="heading">
                 Hi, I'm Anvilly.
-                <div className='subtitle'>
+                <div className='subtitle row'>
                     A graduate software engineer.
-                        <div class="Item">
-                            <a href="https://www.linkedin.com/in/anvilly-huang-92694b142/" target="_blank" rel="noreferrer"><div className='Images linkedin'></div></a>
-                            <a href="https://github.com/zhua633/my-website/" target="_blank" rel="noreferrer"><div className='Images git'></div></a>
+                        <div class="Item_row">
+                            <div class="Item_column">
+                                <a href="https://www.linkedin.com/in/anvilly-huang-92694b142/"><div className='Images linkedin'></div></a>
+                            </div>
+                            <div class="Item_column">
+                                <a href="https://github.com/zhua633/my-website/"><div className='Images git'></div></a>
+                            </div>
                         </div>
-                    <div className='body'>
-                    I'm Anvilly, a fresh mechatronics engineering graduate from the University of Auckland's class of 2022. I've developed a keen interest for programming in my second year of university and aspires to become a front-end engineer. 
-
+                </div>
+                <div className='body'>
+                    I'm Anvilly, a fresh graduate from the University of Auckland's class of 2022 and currently working as a graduate software engineer.
+                    I like music, and sleeps a lot in my spare time.
                     Welcome to my space and feel free to explore my projects as you go.
-
-                    </div>
                 </div>
             </div>
         </section>        )
