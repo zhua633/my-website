caaa77feabdd1fb4a5090538862a18f1cd6e3d4f
zhua633 <zhua633@aucklanduni.ac.nz>
2023-03-31 21:16:47 +1300

    Changed colour schemes and trying animations

--- client/src/Components/Hero/index.jsx
+++ client/src/Components/Hero/index.jsx
@@ -5,24 +5,27 @@ class Hero extends React.Component{
     render(){
         return(
             <section className='hero'>
+            <div className="swing-image"></div>
+            <div className='oval_top_left'></div>
+            <div className='oval_left'></div>
+            <div className='oval_small'></div>
             <div className="heading">
                 Hi, I'm Anvilly.
-                <div className='subtitle row'>
-                    A graduate software engineer.
-                        <div class="Item_row">
-                            <div class="Item_column">
-                                <a href="https://www.linkedin.com/in/anvilly-huang-92694b142/"><div className='Images linkedin'></div></a>
-                            </div>
-                            <div class="Item_column">
-                                <a href="https://github.com/zhua633/my-website/"><div className='Images git'></div></a>
-                            </div>
-                        </div>
-                </div>
+                <p className='subtitle row'> A graduate software engineer.</p>
                 <div className='body'>
                     I'm Anvilly, a fresh graduate from the University of Auckland's class of 2022 and currently working as a graduate software engineer.
                     I like music, and sleeps a lot in my spare time.
                     Welcome to my space and feel free to explore my projects as you go.
                 </div>
+                {/* <div class="Item_row">
+                    <div class="Item_column">
+                        <a href="https://www.linkedin.com/in/anvilly-huang-92694b142/"><div className='Images linkedin'></div></a>
+                    </div>
+                    <div class="Item_column">
+                        <a href="https://github.com/zhua633/my-website/"><div className='Images git'></div></a>
+                    </div>
+                </div> */}
+
             </div>
         </section>        )
     }
